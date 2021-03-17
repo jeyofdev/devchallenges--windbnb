580a8272118ebcc40c2435c500a421851023942a
jeyofdev <jgregoire.pro@gmail.com>
2021-03-17 10:15:30 +0100

    init the context Rooms

---
+++ src/contexts/RoomsContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import stays from '../datas/stays.json';
+
+const RoomsContext = createContext();
+
+const RoomsContextProvider = ({ children }) => {
+    const [rooms] = useState(stays);
+
+    return (
+        <RoomsContext.Provider value={{ rooms }}>
+            {children}
+        </RoomsContext.Provider>
+    );
+};
+
+RoomsContextProvider.propTypes = {
+    children: PropTypes.shape().isRequired,
+};
+
+export default RoomsContextProvider;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RoomsContextProvider from './contexts/RoomsContext';
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <RoomsContextProvider>
+            <App />
+        </RoomsContextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
